19ab78d2c768c6a157678e8f6ddf0574554412d3
Kai Hao <kevin830726@gmail.com>
2018-06-11 01:33:03 +0800

    add noBackspaceCharacter plugin

--- src/client.js
+++ src/client.js
@@ -2,7 +2,14 @@ import highlightShy from './plugins/highlightShy';
 import takeScreenshot from './plugins/takeScreenshot';
 import linkifyJira from './plugins/linkifyJira';
 import linkifyGithub from './plugins/linkifyGithub';
+import noBackspaceCharacter from './plugins/noBackspaceCharacter';
 
-const plugins = [highlightShy, takeScreenshot, linkifyJira, linkifyGithub];
+const plugins = [
+  highlightShy,
+  takeScreenshot,
+  linkifyJira,
+  linkifyGithub,
+  noBackspaceCharacter,
+];
 
 plugins.forEach(plugin => plugin());
---
+++ src/plugins/noBackspaceCharacter.js
@@ -0,0 +1,14 @@
+const MESSAGE_INPUT_ID = 'msg_input';
+const BACKSPACE_REGEX = /\b/g;
+
+function handleInput(e) {
+  e.target.textContent = e.target.textContent.replace(BACKSPACE_REGEX, '');
+}
+
+const noBackspaceCharacter = () => {
+  const messageInput = document.getElementById(MESSAGE_INPUT_ID);
+
+  messageInput.addEventListener('input', handleInput);
+};
+
+export default noBackspaceCharacter;
